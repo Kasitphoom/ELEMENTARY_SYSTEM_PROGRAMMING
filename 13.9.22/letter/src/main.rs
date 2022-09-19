71961a970933e53f1de427b69dceffe94fecdc63
Kasitphoom Thowongs <kasitphoom47@gmail.com>
2022-09-19 20:53:39 +0700

    latest version -date(19/9/22)

--- 13.9.22/letter/src/main.rs
+++ 13.9.22/letter/src/main.rs
@@ -1,5 +1,6 @@
 use std::{io, vec, env, fs};
 use std::io::{BufWriter, Write};
+use std::time::{Instant, Duration};
 
 #[derive(Debug, Clone, Copy)]
 struct freq_table_entry {
@@ -8,8 +9,7 @@ struct freq_table_entry {
     frequency: f64,
 }
 
-// This function will update your freq_table ...
-// maybe it needs an extra argument
+// This function will update your freq_table
 fn unpack_text_line( line: String, freq_table: &mut Vec<freq_table_entry>, non_thai_table: &mut Vec<freq_table_entry> ) -> u64 {
   let mut nc: u64 = 0;
   for i in line.chars() {
@@ -29,7 +29,6 @@ fn read_text_line(filepath: &String) -> String{
   print!("Text to be processed ");
   let mut buffer = fs::read_to_string(filepath).expect("Unable to read file");
   buffer = buffer.trim().to_string();
-  eprintln!("Buffer ({}) [{}]", buffer.len(), buffer );
   buffer
 }
 
@@ -52,54 +51,43 @@ fn check_add_letter( letter: char, freq_table: &mut Vec<freq_table_entry> ) {
   }
 }
 
-// Some other useful functions, eg
-// calc_fractions -- to calculate the fracctions for each letter
 fn calc_percentage( freq_table: &mut Vec<freq_table_entry>, total: u64 ) {
   for e in freq_table.iter_mut() {
     e.frequency = ((e.count as f64) / (total as f64)) * 100.0;
   }
 }
 
-fn print_vec( out_file: &String, freq_table: &Vec<freq_table_entry> , non_thai_table: &Vec<freq_table_entry>, text: String, total: u64 ) {
+fn print_vec( out_file: &String, freq_table: &Vec<freq_table_entry> , non_thai_table: &Vec<freq_table_entry>, text: String, total: u64, duration: Duration ) {
   // create a new file
   let file = fs::File::create(out_file).expect("Unable to create file");
   // write to the file
   let mut file = BufWriter::new(file);
 
   writeln!(file, "Text to be processed:\n{}\n\n", text).expect("Unable to write data");
-
-  println!("Thai Character\n========================================================");
   writeln!(file, "========================================================\n/***************Thai Character***************\\\n").expect("Unable to write to file");
-
-  println!("Letter\tUnicode\tCount\tFrequency (%)");
   writeln!(file, "Letter\tUnicode\tCount\tFrequency (%)").expect("Unable to write to file");
 
   for e in freq_table.iter() {
-    println!("{}\t0{:X}\t{}\t{:.3}%", e.letter, e.letter as u64, e.count, e.frequency );
     let unicode = format!("0{:X}", e.letter as u64);
     writeln!(file, "{:9}\t0{:8}\t{}\t{:.3}%\n", e.letter, unicode, e.count, e.frequency ).expect("Unable to write to file");
   }
-  println!("\nNon-Thai Character\n========================================================");
   writeln!(file, "========================================================\n/***************Non-Thai Character***************\\\n").expect("Unable to write to file");
-
-  println!("Letter\tUnicode\tCount\tFrequency (%)");
   writeln!(file, "Letter\tUnicode\tCount\tFrequency (%)").expect("Unable to write to file");
 
   for e in non_thai_table.iter() {
-    println!("{}\t0{:X}\t{}\t{:.3}%", e.letter, e.letter as u64, e.count, e.frequency );
     let unicode = format!("0{:X}", e.letter as u64);
     writeln!(file, "{:9}\t0{:8}\t{}\t{:.3}%\n", e.letter, unicode, e.count, e.frequency ).expect("Unable to write to file");
   }
   
-  
   let (max_thai, max_non_thai) = add_summary(freq_table, non_thai_table);
+  let time = format!("Time duration to {} miliseconds", duration.as_millis());
+  let total = format!("Total number of characters {}", total);
 
   let sum_head = format!("RESULT SUMMARY: ");
-  writeln!(file, "========================================================\n{}\n{}\n{}",sum_head, max_thai, max_non_thai).expect("Unable to write to file");
+  writeln!(file, "========================================================\n{}\n{}\n{}\n{}\n{}",sum_head, max_thai, max_non_thai, time, total).expect("Unable to write to file");
 
 } 
-// print_report - print table in readable form
-// add_summary - add useful summary - # useful chars, #other spaces, punctuation, non-Thai letter
+
 fn add_summary(freq_table: &Vec<freq_table_entry>, non_thai_table: &Vec<freq_table_entry>) -> (String, String) {
 
   // check most frequent letter in thai
@@ -126,14 +114,15 @@ fn add_summary(freq_table: &Vec<freq_table_entry>, non_thai_table: &Vec<freq_tab
 
   return(max_thai, max_non_thai);
 }
-//     Most frequent letter
+
 
 fn main() {
+  // get time duration of the program
+  let start = Instant::now();
   let args: Vec<String> = env::args().collect();
   let filepath = &args[1];
   let outputpath = &args[2];
 
-  println!("Character Frequencies");
   let mut ix: u64 = 0;
   let mut total = 0;
   let mut nc = 0;
@@ -145,18 +134,17 @@ fn main() {
   let lines = text.lines();
   for l in lines {
     
-    eprintln!("Text [{}], len {}", l , l.len());
  
     nc = unpack_text_line( l.to_string(), &mut cha, &mut non_thai );
     ix += 1;
     
     total += nc;
   }
-  eprintln!("\n{} lines processed", ix );
-  eprintln!("{} characters processed", total );
+  // stop the timer
+  let duration = start.elapsed();
   // fn to Generate frequency fractions
   calc_percentage(&mut cha, total);
   calc_percentage(&mut non_thai, total);
   // fn to Generate report
-  print_vec( outputpath ,&cha, &non_thai, text, total);
+  print_vec( outputpath ,&cha, &non_thai, text, total, duration);
 }
